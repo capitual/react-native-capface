325d1ed4c706171776933ad7929fe31a9d7f168d
Bruno Fialho <brugerf@gmail.com>
2023-09-06 14:00:40 -0300

    fix: refactor code

--- capitual-react-native-capface-sdk.podspec
+++ capitual-react-native-capface-sdk.podspec
@@ -10,38 +10,38 @@ Pod::Spec.new do |s|
   s.homepage     = package["homepage"]
   s.license      = package["license"]
   s.authors      = package["author"]
-
+  
   s.platforms    = { :ios => "11.0" }
   s.source       = { :git => "https://github.com/capitual/react-native-capface.git", :tag => "#{s.version}" }
-
+  
   s.vendored_frameworks = "ios/Frameworks/FaceTecSDK.framework"
   s.source_files = "ios/**/*.{h,m,mm,swift}"
   s.requires_arc = true
-
+  
   # Use install_modules_dependencies helper to install the dependencies if React Native version >=0.71.0.
   # See https://github.com/facebook/react-native/blob/febf6b7f33fdb4904669f99d795eba4c0f95d7bf/scripts/cocoapods/new_architecture.rb#L79.
   if respond_to?(:install_modules_dependencies, true)
     install_modules_dependencies(s)
   else
-  s.dependency "React-Core"
-  
-  s.ios.resource_bundles = {
-    'ReactNativeFaceTecSdk' => ['ios/SupportingFiles/*.xcassets']
-  }
-
-  # Don't install the dependencies when we run `pod install` in the old architecture.
-  if ENV['RCT_NEW_ARCH_ENABLED'] == '1' then
-    s.compiler_flags = folly_compiler_flags + " -DRCT_NEW_ARCH_ENABLED=1"
-    s.pod_target_xcconfig    = {
+    s.dependency "React-Core"
+    
+    s.ios.resource_bundles = {
+      'ReactNativeFaceTecSdk' => ['ios/SupportingFiles/*.xcassets']
+    }
+    
+    # Don't install the dependencies when we run `pod install` in the old architecture.
+    if ENV['RCT_NEW_ARCH_ENABLED'] == '1' then
+      s.compiler_flags = folly_compiler_flags + " -DRCT_NEW_ARCH_ENABLED=1"
+      s.pod_target_xcconfig    = {
         "HEADER_SEARCH_PATHS" => "\"$(PODS_ROOT)/boost\"",
         "OTHER_CPLUSPLUSFLAGS" => "-DFOLLY_NO_CONFIG -DFOLLY_MOBILE=1 -DFOLLY_USE_LIBCPP=1",
         "CLANG_CXX_LANGUAGE_STANDARD" => "c++17"
-    }
-    s.dependency "React-Codegen"
-    s.dependency "RCT-Folly"
-    s.dependency "RCTRequired"
-    s.dependency "RCTTypeSafety"
-    s.dependency "ReactCommon/turbomodule/core"
-   end
-  end    
+      }
+      s.dependency "React-Codegen"
+      s.dependency "RCT-Folly"
+      s.dependency "RCTRequired"
+      s.dependency "RCTTypeSafety"
+      s.dependency "ReactCommon/turbomodule/core"
+    end
+  end
 end
--- ios/Processors/Helpers/ThemeUtils.swift
+++ ios/Processors/Helpers/ThemeUtils.swift
@@ -10,71 +10,71 @@ import Foundation
 import FaceTecSDK
 
 public class ThemeUtils {
-  private func themeAndKeyIsntExists(_ key: String) -> Bool {
-    return Config.Theme == nil || Config.Theme?[key] == nil;
+    private func themeAndKeyIsntExists(_ key: String) -> Bool {
+        return Config.Theme == nil || Config.Theme?[key] == nil;
     }
-
+    
     private func parseUIColor(_ key: String, defaultColor: UIColor) -> UIColor {
         let color = (Config.Theme?[key] as? String) ?? "";
-    if (color.isEmpty) {
+        if (color.isEmpty) {
             return defaultColor;
         }
-    return UIColor(hexString: color);
+        return UIColor(hexString: color);
     }
-
-  private func convertHexColorsToUIColors(_ hexColors: [String?]) -> [UIColor] {
-    var colors = [UIColor]();
-    if hexColors.isEmpty {
-      return colors;
-    }
-
+    
+    private func convertHexColorsToUIColors(_ hexColors: [String?]) -> [UIColor] {
+        var colors = [UIColor]();
+        if hexColors.isEmpty {
+            return colors;
+        }
+        
         for hexColor in hexColors {
             if (hexColor != nil && hexColor != "") {
                 colors.append(UIColor(hexString: hexColor!));
             }
         }
-
+        
         if (colors.count == 1) {
             colors.append(colors[0]);
         }
         return colors;
     }
-
+    
     private func convertHexColorsToCGColors(_ hexColors: [String?]) -> [CGColor] {
-    var colors = [CGColor]();
+        var colors = [CGColor]();
         let defaultColor = UIColor(hexString: "#026ff4").cgColor;
         let defaultColors = [defaultColor, defaultColor];
         if (hexColors.isEmpty) {
             return defaultColors;
         }
-
+        
         for hexColor in hexColors {
             if (hexColor != nil && hexColor != "") {
                 let cgColor = UIColor(hexString: hexColor!).cgColor;
                 colors.append(cgColor);
             }
         }
-
+        
         if (colors.count == 1) {
             colors.append(colors[0]);
         }
         return colors;
     }
-
+    
     private func handleLocationGradient(_ locations: [Int?]) -> [NSNumber] {
-    let defaultLocations: [NSNumber] = [0, 1];
+        let defaultLocations: [NSNumber] = [0, 1];
         if (locations.count != 2) {
             return defaultLocations;
         }
-
+        
         for location in locations {
-      if (location == nil || (location! < 0 || location! > 1)) {
+            if (location == nil || (location! < 0 || location! > 1)) {
                 return defaultLocations;
             }
         }
         return locations as! [NSNumber];
     }
-
+    
     private func handleCGPoints(_ points: NSDictionary?, defaultPoints: CGPoint) -> CGPoint {
         if (points == nil) {
             return defaultPoints;
@@ -83,118 +83,118 @@ public class ThemeUtils {
         let yPoint = points?["y"] as? Double ?? defaultPoints.y;
         return CGPoint.init(x: xPoint, y: yPoint);
     }
-
+    
     private func handleCAGradientProperties(_ acGradient: NSDictionary) -> CAGradientLayer {
         let gradiant = CAGradientLayer.init();
         let properties = ["colors", "locations", "startPoint", "endPoint"];
         for property in properties {
-        if (acGradient[property] != nil) {
+            if (acGradient[property] != nil) {
                 switch property {
-                    case "colors": do {
-                        gradiant.colors = self.convertHexColorsToCGColors((acGradient[property] as? [String?] ?? []));
-                        break;
-                    }
-                    case "locations": do {
-            gradiant.locations = self.handleLocationGradient((acGradient[property] as? [Int?] ?? []));
-                        break;
-                    }
-                    case "startPoint": do {
-                        gradiant.startPoint = self.handleCGPoints((acGradient[property] as? NSDictionary), defaultPoints: CGPoint.init(x: 0, y: 0));
-                        break;
-                    }
-                    case "endPoint": do {
-                        gradiant.endPoint = self.handleCGPoints((acGradient[property] as? NSDictionary), defaultPoints: CGPoint.init(x: 1, y: 0));
-                        break;
-                    }
-                    default: do {
-                        break;
-                    }
+                case "colors": do {
+                    gradiant.colors = self.convertHexColorsToCGColors((acGradient[property] as? [String?] ?? []));
+                    break;
+                }
+                case "locations": do {
+                    gradiant.locations = self.handleLocationGradient((acGradient[property] as? [Int?] ?? []));
+                    break;
+                }
+                case "startPoint": do {
+                    gradiant.startPoint = self.handleCGPoints((acGradient[property] as? NSDictionary), defaultPoints: CGPoint.init(x: 0, y: 0));
+                    break;
+                }
+                case "endPoint": do {
+                    gradiant.endPoint = self.handleCGPoints((acGradient[property] as? NSDictionary), defaultPoints: CGPoint.init(x: 1, y: 0));
+                    break;
+                }
+                default: do {
+                    break;
+                }
                 }
             }
         }
         return gradiant;
     }
-
-  public func handleBorderRadius(_ key: String) -> Int32 {
-    let defaultBorderRadius: Int32 = 10;
+    
+    public func handleBorderRadius(_ key: String) -> Int32 {
+        let defaultBorderRadius: Int32 = 10;
         if (self.themeAndKeyIsntExists(key)) {
             return defaultBorderRadius;
         }
-
-    let borderRadius = (Config.Theme?[key] as? Int32) ?? defaultBorderRadius
-    return borderRadius < 0 ? defaultBorderRadius : borderRadius;
+        
+        let borderRadius = (Config.Theme?[key] as? Int32) ?? defaultBorderRadius
+        return borderRadius < 0 ? defaultBorderRadius : borderRadius;
     }
-
-  public func handleColor(_ key: String) -> UIColor {
+    
+    public func handleColor(_ key: String) -> UIColor {
         let defaultColor = UIColor(hexString: "#ffffff");
         if (self.themeAndKeyIsntExists(key)) {
             return defaultColor;
         }
         return self.parseUIColor(key, defaultColor: defaultColor);
     }
-
-  public func handleColor(_ key: String, defaultColor: String) -> UIColor {
+    
+    public func handleColor(_ key: String, defaultColor: String) -> UIColor {
         if (self.themeAndKeyIsntExists(key)) {
             return UIColor(hexString: defaultColor);
         }
         return self.parseUIColor(key, defaultColor: UIColor(hexString: defaultColor));
     }
-
-  public func handleSimpleLinearGradient(_ key: String) -> [UIColor] {
+    
+    public func handleSimpleLinearGradient(_ key: String) -> [UIColor] {
         let color = UIColor(hexString: "#ffffff");
         let defaultColors = [color, color];
         if (self.themeAndKeyIsntExists(key)) {
             return defaultColors;
         }
         let hexColors = Config.Theme?[key] as? [String?] ?? [];
-    let colors = self.convertHexColorsToUIColors(hexColors);
+        let colors = self.convertHexColorsToUIColors(hexColors);
         if (colors.isEmpty) {
             return defaultColors;
         }
         return colors;
     }
-
+    
     public func handleCAGradient(_ key: String) -> CAGradientLayer {
-    let defaultColor = UIColor(hexString: "#026ff4").cgColor;
+        let defaultColor = UIColor(hexString: "#026ff4").cgColor;
         let caGradient = CAGradientLayer.init();
         caGradient.colors = [defaultColor, defaultColor];
         caGradient.locations = [0, 1];
         caGradient.startPoint = CGPoint.init(x: 0, y: 0);
         caGradient.endPoint = CGPoint.init(x: 1, y: 0);
-
+        
         if (self.themeAndKeyIsntExists(key)) {
             return caGradient;
         }
-
+        
         let gradiant = Config.Theme?[key] as? NSDictionary;
         if (gradiant != nil) {
             return self.handleCAGradientProperties(gradiant!);
         }
         return caGradient;
     }
-
+    
     public func handleMessage(_ key: String, child: String, defaultMessage: String) -> String {
         if (self.themeAndKeyIsntExists(key)) {
             return defaultMessage;
         }
         let rootObject = Config.Theme?[key] as? NSDictionary;
-    return rootObject?[child] as? String ?? defaultMessage;
+        return rootObject?[child] as? String ?? defaultMessage;
     }
-
+    
     public func handleImage(_ key: String, defaultImage: String) -> UIImage? {
         if (self.themeAndKeyIsntExists(key)) {
             return UIImage(named: defaultImage);
         }
-
+        
         let imageName = (Config.Theme?[key] as? String) ?? "";
-    if (imageName.isEmpty) {
+        if (imageName.isEmpty) {
             return UIImage(named: defaultImage);
         }
         return UIImage(named: imageName);
     }
-
+    
     public func handleButtonLocation(_ key: String) -> FaceTecCancelButtonLocation {
-    let defaultLocation = FaceTecCancelButtonLocation.topRight;
+        let defaultLocation = FaceTecCancelButtonLocation.topRight;
         if (self.themeAndKeyIsntExists(key)) {
             return defaultLocation;
         }
@@ -202,29 +202,29 @@ public class ThemeUtils {
         if (buttonLocation.isEmpty) {
             return defaultLocation;
         }
-
+        
         switch (buttonLocation) {
-            case "TOP_RIGHT": do {
-        return FaceTecCancelButtonLocation.topRight;
-            }
-            case "TOP_LEFT": do {
-                return FaceTecCancelButtonLocation.topLeft;
-            }
-            case "DISABLED": do {
-                return FaceTecCancelButtonLocation.disabled;
-            }
-            default: do {
-                return defaultLocation;
-            }
+        case "TOP_RIGHT": do {
+            return FaceTecCancelButtonLocation.topRight;
+        }
+        case "TOP_LEFT": do {
+            return FaceTecCancelButtonLocation.topLeft;
+        }
+        case "DISABLED": do {
+            return FaceTecCancelButtonLocation.disabled;
+        }
+        default: do {
+            return defaultLocation;
+        }
         }
     }
-
+    
     public func handleStatusBarStyle(_ key: String) -> UIStatusBarStyle {
         var defaultStatusBar = UIStatusBarStyle.default;
         if #available(iOS 13, *) {
             defaultStatusBar = UIStatusBarStyle.darkContent;
         }
-
+        
         if (self.themeAndKeyIsntExists(key)) {
             return defaultStatusBar;
         }
@@ -232,24 +232,24 @@ public class ThemeUtils {
         if (statusBarColor.isEmpty) {
             return defaultStatusBar;
         }
-
+        
         if #available(iOS 13, *) {
             switch (statusBarColor) {
-                case "DARK_CONTENT": do {
-                    return defaultStatusBar;
-                }
-                case "LIGHT_CONTENT": do {
-                    return UIStatusBarStyle.lightContent;
-                }
-                case "DEFAULT": do {
-                    return UIStatusBarStyle.default;
-                }
-                default: do {
-                    return defaultStatusBar;
-                }
+            case "DARK_CONTENT": do {
+                return defaultStatusBar;
             }
-    }
-
+            case "LIGHT_CONTENT": do {
+                return UIStatusBarStyle.lightContent;
+            }
+            case "DEFAULT": do {
+                return UIStatusBarStyle.default;
+            }
+            default: do {
+                return defaultStatusBar;
+            }
+            }
+        }
+        
         return defaultStatusBar;
     }
 }
