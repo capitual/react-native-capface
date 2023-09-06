d56a0e8eaad4be68d546d8c0f50c7e15e2986510
Bruno Fialho <brugerf@gmail.com>
2023-09-06 14:35:55 -0300

    feat: ios modules (#3)
    * feat: add ios module
    
    * chore: update facetec sdk
    
    * fix: refactor code
    
    * fix: authenticate message

--- ios/ReactNativeCapfaceSdk-Bridging-Header.h
+++ ios/ReactNativeCapfaceSdk-Bridging-Header.h
@@ -1,2 +1,11 @@
+//
+//  ReactNativeCapfaceSdk-Bridging-Header.h
+//  ReactNativeCapfaceSdk
+//
+//  Created by Nayara Dias, Bruno Fialho and Daniel Sansão on 04/09/23.
+//  Copyright © 2023 Capitual. All rights reserved.
+//
+
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 #import <React/RCTViewManager.h>
--- ios/ReactNativeCapfaceSdk.mm
+++ ios/ReactNativeCapfaceSdk.mm
@@ -1,14 +1,37 @@
+//
+//  ReactNativeCapfaceSdk.m
+//  ReactNativeCapfaceSdk
+//
+//  Created by Nayara Dias, Bruno Fialho and Daniel Sansão on 04/09/23.
+//  Copyright © 2023 Capitual. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTViewManager.h>
 
-@interface RCT_EXTERN_MODULE(ReactNativeCapfaceSdk, NSObject)
+@interface RCT_EXTERN_MODULE(ReactNativeCapfaceSdk, RCTEventEmitter)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-
-+ (BOOL)requiresMainQueueSetup
-{
-  return NO;
-}
+RCT_EXTERN_METHOD(initializeSdk:(NSDictionary *)params
+                  headers:(NSDictionary *)headers
+                  callback:(RCTResponseSenderBlock)callback)
+RCT_EXTERN_METHOD(handleLivenessCheck:(NSDictionary *)data
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(handleEnrollUser:(NSDictionary *)data
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(handleAuthenticateUser:(NSDictionary *)data
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(handlePhotoIDMatch:(NSDictionary *)data
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(handlePhotoIDScan:(NSDictionary *)data
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(handleTheme:(NSDictionary *)options)
+RCT_EXTERN_METHOD(supportedEvents)
 
 @end
