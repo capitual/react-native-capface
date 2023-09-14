386a17fc841240ef16f9235e610724f527d83461
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-09-14 20:30:06 -0300

    feat: created `convertToHexColor` function to manager colors

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,6 +1,15 @@
-function isHexColor(hexColor: string): boolean {
-  const regexToCheckIfHexColor = /^#(([0-9A-Fa-f]{2}){3,4}|[0-9A-Fa-f]{3})$/;
-  return regexToCheckIfHexColor.test(hexColor);
+type ColorType = 'HEX' | 'RGB' | 'RGBA' | 'HSL' | 'HSLA';
+type ColorTableType = Exclude<ColorType, 'HEX'>;
+
+function isColor(type: ColorType, color: string): boolean {
+  const regexsColors: Record<ColorType, RegExp> = {
+    HEX: /^#(([0-9A-Fa-f]{2}){3,4}|[0-9A-Fa-f]{3})$/,
+    HSL: /[Hh][Ss][Ll][(](((([\d]{1,3}|[\d%]{2,4})[,]{0,1})[\s]*){3})[)]/gm,
+    HSLA: /[Hh][Ss][Ll][Aa][(](((([\d]{1,3}|[\d%]{2,4}|[\d.]{1,3})[,]{0,1})[\s]*){4})[)]/gm,
+    RGB: /[Rr][Gg][Bb][(](((([\d]{1,3})[,]{0,1})[\s]*){3})[)]/gm,
+    RGBA: /[Rr][Gg][Bb][Aa][(](((([\d]{1,3}|[\d.]{1,3})[,]{0,1})[\s]*){4})[)]/gm,
+  };
+  return regexsColors[type].test(color);
 }
 
 function isNumberBetween(value: number, min: number, max: number): boolean {
@@ -25,27 +34,26 @@ function calculateHslHex(
     .padStart(2, '0');
 }
 
-type ColorTableType = 'RGB' | 'RGBA' | 'HSL' | 'HSLA';
-
 function isBetweenLimits(type: ColorTableType, colors: number[]): boolean {
   const hasAlpha = type === 'HSLA' || type === 'RGBA';
   const isRgb = type === 'RGBA' || type === 'RGB';
 
   const [firstColor, secondColor, thirdColor, alpha] = colors;
-  const isValid =
+  const isValidColor =
     isNumberBetween(firstColor!, 0, isRgb ? 255 : 360) &&
     isNumberBetween(secondColor!, 0, isRgb ? 255 : 100) &&
     isNumberBetween(thirdColor!, 0, isRgb ? 255 : 100);
 
-  if (hasAlpha) return isValid && isNumberBetween(alpha!, 0, 1);
-  return isValid;
+  if (hasAlpha) return isValidColor && isNumberBetween(alpha!, 0, 1);
+  return isValidColor;
 }
 
 function getColorTable(type: ColorTableType, color: string): number[] | null {
   const hasAlpha = type === 'HSLA' || type === 'RGBA';
+  const isHsl = type === 'HSL' || type === 'HSLA';
   const COLOR_LENGTH = hasAlpha ? 4 : 3;
 
-  const regexToRemoveCharacters = hasAlpha ? /[A-Z()\s%]/gi : /[A-Z()\s]/gi;
+  const regexToRemoveCharacters = isHsl ? /[A-Z()\s%]/gi : /[A-Z()\s]/gi;
   const tableValues = color
     .toLowerCase()
     .replace(regexToRemoveCharacters, '')
@@ -53,55 +61,20 @@ function getColorTable(type: ColorTableType, color: string): number[] | null {
 
   if (tableValues.length !== COLOR_LENGTH) return null;
 
-  const [firstColor, secondColor, thirdColor] = tableValues;
+  const tableValuesAsNumber = tableValues.map((value, index) => {
+    if (hasAlpha && index === COLOR_LENGTH - 1) return Number.parseFloat(value);
+    return Number.parseInt(value, 10);
+  });
+
   if (hasAlpha && tableValues.every((value) => !!value)) {
-    return tableValues.map((value, index) => {
-      if (index !== COLOR_LENGTH - 1) return Number.parseFloat(value);
-      return Number.parseInt(value, 10);
-    });
+    return tableValuesAsNumber;
   }
 
-  const hasEveryColors = !!firstColor && !!secondColor && !!thirdColor;
-  return hasEveryColors
-    ? tableValues.map((value) => Number.parseInt(value, 10))
-    : null;
+  return tableValues.every((value) => !!value) ? tableValuesAsNumber : null;
 }
 
-export function rgbToHex(rgbColor: string): string | null {
-  const colorTable = getColorTable('RGB', rgbColor);
-  if (!colorTable) return null;
-
-  if (!isBetweenLimits('RGB', colorTable)) return null;
-  const [red, green, blue] = colorTable;
-
-  const byteNumbers = (1 << 24) | (red! << 16) | (green! << 8) | blue!;
-  const hexColor = `#${byteNumbers.toString(16).slice(1).toUpperCase()}`;
-
-  if (!isHexColor(hexColor)) return null;
-  return hexColor;
-}
-
-export function rgbaToHex(rgbaColor: string): string | null {
-  const colorTable = getColorTable('RGBA', rgbaColor);
-  if (!colorTable) return null;
-
-  if (!isBetweenLimits('RGBA', colorTable)) return null;
-  const [red, green, blue, alpha] = colorTable;
-
-  const alphaNumericString = (((alpha! as any) * 255) | (1 << 8))
-    .toString(16)
-    .slice(1);
-  const byteNumbers = (1 << 24) | (red! << 16) | (green! << 8) | blue!;
-  const hexColor = `#${byteNumbers
-    .toString(16)
-    .slice(1)}${alphaNumericString}`.toUpperCase();
-
-  if (!isHexColor(hexColor)) return null;
-  return hexColor;
-}
-
-export function formatHexColor(hexColor: string): string | null {
-  if (!isHexColor(hexColor)) return null;
+function formatHexColor(hexColor: string): string | null {
+  if (!isColor('HEX', hexColor)) return null;
   const MIN_LENGTH_HEX_COLOR = 4;
   const MIDDLE_LENGTH_HEX_COLOR = 7;
   const MAX_LENGTH_HEX_COLOR = 9;
@@ -117,10 +90,46 @@ export function formatHexColor(hexColor: string): string | null {
   const greenHexColor = secondChar!.repeat(2);
   const blueHexColor = thirdChar!.repeat(2);
 
-  return `#${redHexColor}${greenHexColor}${blueHexColor}`.toUpperCase();
+  const color = `#${redHexColor}${greenHexColor}${blueHexColor}`.toUpperCase();
+
+  if (!isColor('HEX', color)) return null;
+  return color;
 }
 
-export function hslToHex(hslColor: string): string | null {
+function rgbToHex(rgbColor: string): string | null {
+  const colorTable = getColorTable('RGB', rgbColor);
+  if (!colorTable) return null;
+
+  if (!isBetweenLimits('RGB', colorTable)) return null;
+
+  const [red, green, blue] = colorTable;
+  const byteNumbers = (1 << 24) | (red! << 16) | (green! << 8) | blue!;
+  const hexColor = `#${byteNumbers.toString(16).slice(1).toUpperCase()}`;
+
+  if (!isColor('HEX', hexColor)) return null;
+  return hexColor;
+}
+
+function rgbaToHex(rgbaColor: string): string | null {
+  const colorTable = getColorTable('RGBA', rgbaColor);
+  if (!colorTable) return null;
+
+  if (!isBetweenLimits('RGBA', colorTable)) return null;
+  const [red, green, blue, alpha] = colorTable;
+
+  const alphaNumeric = (((alpha! as any) * 255) | (1 << 8))
+    .toString(16)
+    .slice(1);
+  const byteNumbers = (1 << 24) | (red! << 16) | (green! << 8) | blue!;
+  const hexColor = `#${byteNumbers
+    .toString(16)
+    .slice(1)}${alphaNumeric}`.toUpperCase();
+
+  if (!isColor('HEX', hexColor)) return null;
+  return hexColor;
+}
+
+function hslToHex(hslColor: string): string | null {
   const colorTable = getColorTable('HSL', hslColor);
   if (!colorTable) return null;
 
@@ -134,11 +143,11 @@ export function hslToHex(hslColor: string): string | null {
   const hexColor =
     `#${redHexColor}${greenHexColor}${blueHexColor}`.toUpperCase();
 
-  if (!isHexColor(hexColor)) return null;
+  if (!isColor('HEX', hexColor)) return null;
   return hexColor;
 }
 
-export function hslaToHex(hslaColor: string): string | null {
+function hslaToHex(hslaColor: string): string | null {
   const colorTable = getColorTable('HSLA', hslaColor);
   if (!colorTable) return null;
 
@@ -155,6 +164,15 @@ export function hslaToHex(hslaColor: string): string | null {
   const hexColor =
     `#${redHexColor}${greenHexColor}${blueHexColor}${alphaValue}`.toUpperCase();
 
-  if (!isHexColor(hexColor)) return null;
+  if (!isColor('HEX', hexColor)) return null;
   return hexColor;
+}
+
+export function convertToHexColor(color: string): string | null {
+  if (isColor('HEX', color)) return formatHexColor(color);
+  if (isColor('RGB', color)) return rgbToHex(color);
+  if (isColor('RGBA', color)) return rgbaToHex(color);
+  if (isColor('HSL', color)) return hslToHex(color);
+  if (isColor('HSLA', color)) return hslaToHex(color);
+  return null;
 }
