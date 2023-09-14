e8438077ec92ccdfb159776cb6b05d63c159022e
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-09-14 11:55:57 -0300

    feat: created convert to rgba color to hexadecimal color helper

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,7 +1,6 @@
-function parseFloat(value?: string): number {
+function parseInt(value?: string): number {
   if (!value || Number.isNaN(value)) return -1;
-
-  return Number.parseFloat(value);
+  return Number.parseInt(value, 10);
 }
 
 function isHexColor(hexColor: string): boolean {
@@ -9,16 +8,12 @@ function isHexColor(hexColor: string): boolean {
   return regexToCheckIfHexColor.test(hexColor);
 }
 
-function isRgbNumber(numberColor: number): boolean {
-  if (numberColor !== 0 && !numberColor) return false;
-
-  const MIN_NUMBER_COLOR = 0;
-  const MAX_NUMBER_COLOR = 255;
-
-  return numberColor >= MIN_NUMBER_COLOR && numberColor <= MAX_NUMBER_COLOR;
+function isNumberBetween(value: number, min: number, max: number): boolean {
+  if (value !== 0 && !value) return false;
+  return value >= min && value <= max;
 }
 
-export function rgbToHexadecimal(rgbColor: string): string | null {
+export function rgbToHex(rgbColor: string): string | null {
   const LENGTH_OF_RGB_COLOR = 3;
 
   const regexToRemoveCharacters = /[A-Z()\s]/gi;
@@ -33,22 +28,63 @@ export function rgbToHexadecimal(rgbColor: string): string | null {
   const allColorsExists = !!red && !!green && !!blue;
   if (!allColorsExists) return null;
 
-  const redNumber = parseFloat(red);
-  const greenNumber = parseFloat(green);
-  const blueNumber = parseFloat(blue);
+  const redNumber = parseInt(red);
+  const greenNumber = parseInt(green);
+  const blueNumber = parseInt(blue);
 
   if (
-    !isRgbNumber(redNumber) ||
-    !isRgbNumber(greenNumber) ||
-    !isRgbNumber(blueNumber)
+    !isNumberBetween(redNumber, 0, 255) ||
+    !isNumberBetween(greenNumber, 0, 255) ||
+    !isNumberBetween(blueNumber, 0, 255)
   ) {
     return null;
   }
 
   const byteNumbers =
     (1 << 24) | (redNumber << 16) | (greenNumber << 8) | blueNumber;
+  const hexCharacters = byteNumbers.toString(16).slice(1).toUpperCase();
+  const hexColor = `#${hexCharacters}`;
 
-  const hexColor = '#' + byteNumbers.toString(16).slice(1).toUpperCase();
+  if (!isHexColor(hexColor)) return null;
+  return hexColor;
+}
+
+export function rgbaToHex(rgbaColor: string): string | null {
+  const LENGTH_OF_RGBA_COLOR = 4;
+
+  const regexToRemoveCharacters = /[A-Z()\s]/gi;
+  const redGreenAndBlueNumbers = rgbaColor
+    .toLowerCase()
+    .replace(regexToRemoveCharacters, '')
+    .split(',');
+
+  if (redGreenAndBlueNumbers.length !== LENGTH_OF_RGBA_COLOR) return null;
+
+  const [red, green, blue, alpha] = redGreenAndBlueNumbers;
+  const allColorsExists = !!red && !!green && !!blue && !!alpha;
+  if (!allColorsExists) return null;
+
+  const redNumber = parseInt(red);
+  const greenNumber = parseInt(green);
+  const blueNumber = parseInt(blue);
+  const alphaNumber = parseInt(alpha);
+
+  if (
+    !isNumberBetween(redNumber, 0, 255) ||
+    !isNumberBetween(greenNumber, 0, 255) ||
+    !isNumberBetween(blueNumber, 0, 255) ||
+    !isNumberBetween(alphaNumber, 0, 1)
+  ) {
+    return null;
+  }
+
+  const redNumericString = redNumber.toString(16);
+  const greenNumericString = greenNumber.toString(16);
+  const blueNumericString = blueNumber.toString(16);
+  const alphaNumericString = alphaNumber.toString(16).slice(1);
+
+  const hexColor =
+    `#${redNumericString}${greenNumericString}${blueNumericString}${alphaNumericString}`.toUpperCase();
 
   if (!isHexColor(hexColor)) return null;
   return hexColor;
@@ -67,5 +103,5 @@ export function formatHexColor(hexColor: string): string | null {
   if (hexColor.length !== MIN_LENGTH_HEX_COLOR) return null;
 
   const [firstChar, secondChar, thirdChar] = hexColor.slice(1);
-  return `#${firstChar}${firstChar}${secondChar}${secondChar}${thirdChar}${thirdChar}`;
+  return `#${firstChar}${firstChar}${secondChar}${secondChar}${thirdChar}${thirdChar}`.toUpperCase();
 }
